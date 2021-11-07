affe46fd0ada5e359eddee3756dad5df1d4ab20a
abcdef <mattkerschbaum@Hotmail.com>
2021-11-07 06:24:56 -0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+setuptools.setup(name='goopy_misc',
+version='1.0',
+description='Goo miscellaneous library',
+url='#',
+author='goo',
+install_requires=['loguru'],
+author_email='',
+packages=setuptools.find_packages(),
+zip_safe=False)
